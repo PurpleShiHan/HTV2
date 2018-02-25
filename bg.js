1ee5454f06b7fb58d0f195d30f0cf6bbac260f52
Thomas Ma <thomas.ma@mail.utoronto.ca>
2018-02-25 00:42:38 -0500

    added timer

--- bg.js
+++ bg.js
@@ -1,9 +1,76 @@
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-	if (changeInfo.status == 'complete' && window.localStorage.getItem(tab.url) != null) {
+	if (changeInfo.status == 'complete' && checkBlacklisted(tab.url)) {
 		chrome.tabs.update(tab.id, {url: "mathscreen.html"});
 	}
 });
 
-function redirect() {
-	location.replace("google.com");
+function checkBlacklisted(url) {
+	for (var i=0; i<localStorage.length; i++) {
+		if (localStorage.getItem(localStorage.key(i)) == "blocked site") {
+			if (url.includes(localStorage.getItem(localStorage.key(i))) {
+				return true;
+			}
+		}
+	}
+	return false;
 }
+
+var History = {};
+
+chrome.browserAction.setBadgeText({ 'text': '?'});
+chrome.browserAction.setBadgeBackgroundColor({ 'color': "#777" });
+
+function Update(t, tabId, url) {
+  if (!url) {
+    return;
+  }
+  if (tabId in History) {
+    if (url == History[tabId][0][1]) {
+      return;
+    }
+  } else {
+    History[tabId] = [];
+  }
+  History[tabId].unshift([t, url]);
+
+  var history_limit = parseInt(localStorage["history_size"]);
+  if (! history_limit) {
+    history_limit = 23;
+  }
+  while (History[tabId].length > history_limit) {
+    History[tabId].pop();
+  }
+
+  chrome.browserAction.setBadgeText({ 'tabId': tabId, 'text': '0:00'});
+  chrome.browserAction.setPopup({ 'tabId': tabId, 'popup': "popup.html#tabId=" + tabId});
+}
+
+function HandleUpdate(tabId, changeInfo, tab) {
+  Update(new Date(), tabId, changeInfo.url);
+}
+
+function HandleRemove(tabId, removeInfo) {
+  delete History[tabId];
+}
+
+function HandleReplace(addedTabId, removedTabId) {
+  var t = new Date();
+  delete History[removedTabId];
+  chrome.tabs.get(addedTabId, function(tab) {
+    Update(t, addedTabId, tab.url);
+  });
+}
+
+function UpdateBadges() {
+  var now = new Date();
+  for (tabId in History) {
+    var description = FormatDuration(now - History[tabId][0][0]);
+    chrome.browserAction.setBadgeText({ 'tabId': parseInt(tabId), 'text': description});
+  }
+}
+
+setInterval(UpdateBadges, 1000);
+
+chrome.tabs.onUpdated.addListener(HandleUpdate);
+chrome.tabs.onRemoved.addListener(HandleRemove);
+chrome.tabs.onReplaced.addListener(HandleReplace);
---
+++ common.js
@@ -0,0 +1,20 @@
+"use strict";
+
+function FormatDuration(d) {
+  if (d < 0) {
+    return "?";
+  }
+  var divisor = d < 3600000 ? [60000, 1000] : [3600000, 60000];
+  function pad(x) {
+    return x < 10 ? "0" + x : x + "999";
+  }
+  if (Math.floor(d / divisor[0]) == 1){
+  	return "DONE!";
+  }
+  else{
+  	return Math.floor(d / divisor[0]) + ":" + pad(Math.floor((d % divisor[0]) / divisor[1]));
+  }
+  
+}
+
+exports.FormatDuration = FormatDuration;
