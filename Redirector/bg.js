386be686d3f9bfa81954d7409bde55f8b4902539
Thomas Ma <thomas.ma@mail.utoronto.ca>
2018-02-24 21:15:03 -0500

    got storage of urls working

--- Redirector/bg.js
+++ Redirector/bg.js
@@ -1,6 +1,5 @@
-var host = "file:///C:/Users/Thomas%20Ma/Documents/ChromeExtensions/HTV2/Redirector/mathscreen.html";
 website = "*://google.ca/*";
-website2 = "*://www.google.ca/*"
+website2 = "*://www.google.ca/*";
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
          return {redirectUrl: chrome.extension.getURL("mathscreen.html")};
--- popup.js
+++ popup.js
@@ -1,6 +1,29 @@
 chrome.tabs.getSelected(null, function(tab) {
     document.getElementById('currentLink').innerHTML = tab.url;
-    chrome.storage.sync.set({'foo': 'hello', 'bar': 'hi'}, function() {
-      console.log('Settings saved');
-    });
+});
+
+function blacklistHandler(e){
+	chrome.tabs.getSelected(null, function(tab) {
+		window.localStorage.setItem(tab.url, tab.url);
+		document.getElementById("currentLink").innerHTML = "Link blacklisted!";
+	});
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById("blacklistbutton").addEventListener('click', blacklistHandler);
+});
+
+function whitelistHandler(e) {
+	chrome.tabs.getSelected(null, function(tab) {
+		if (window.localStorage.getItem(tab.url) != null) {
+			window.localStorage.removeItem(tab.url);
+			document.getElementById("currentLink").innerHTML = "Link removed!";
+		} else {
+			document.getElementById("currentLink").innerHTML = "Link not blacklisted!";
+		}
+	});
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById("whitelistbutton").addEventListener('click', whitelistHandler);
 });
